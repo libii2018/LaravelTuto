c5678063a72a7ad91906506beada4427040b0fe0
Libii <libiijoseph@gmail.com>
2024-06-12 18:18:34 +0100

    First version tuto

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,18 @@
+@extends('base')
+
+@section('title','Acceuil du blog')
+
+@section('content')
+    @foreach ($posts as $post)
+        <article>
+            <h2>{{ $post->title }}</h2>
+            <p>
+                {{ $post->content }}
+            </p>
+            <p>
+                <a href="{{ route('blog.show', ['slug' => $post->slug, 'id' => $post->id]) }}" class="btn btn-primary">Lire la suite</a>
+            </p>
+        </article>
+    @endforeach
+    {{ $posts->links() }}
+@endsection
